b68cfc8f600bf0144b2f602c4557b7dea1ffad26
ZorudaRinku <stronger6@gmail.com>
2025-01-27 11:25:07 -0700

    Item Icons

--- Assets/Scripts/Items/Foresight.cs
+++ Assets/Scripts/Items/Foresight.cs
@@ -5,42 +5,39 @@ using UnityEngine;
 public class Foresight : MonoBehaviour, IItem
 {
     [SerializeField] public Items itemType;
-    private GameObject textPosition;
-    private GameObject GameManager;
-    private TMP_Text foresightText;
-    private Animator textAnimator;
+    private GameObject _gameManager;
+    private TMP_Text _headerText;
+    private Animator _headerTextAnimator;
     private string itemName;
     private string itemDescription;
 
     public void Start()
     {
         // initialization phase
-        textAnimator = GetComponent<Animator>();
-        foresightText = GetComponent<TMP_Text>();
-        GameManager = GameObject.Find("GameManager");
-        textPosition = GameObject.Find("HeaderTextPosition");
+        _headerTextAnimator = GameObject.Find("HeaderText").GetComponent<Animator>();
+        _headerText = GameObject.Find("HeaderText").GetComponent<TextMeshProUGUI>();
+        _gameManager = GameObject.Find("GameManager");
         itemName = itemType.itemName;
         itemDescription = itemType.itemDescription;
-        transform.position = textPosition.transform.position; // sets foresight text position
     } // Start
 
     public void UseItem()
     {
         // set the text for foresight
-        foresightText.text = GenerateForesightText();
+        _headerText.text = GenerateForesightText();
 
         // play text fade in animation
-        textAnimator.SetTrigger("TriggerFadeIn");
+        _headerTextAnimator.SetTrigger("TriggerFadeIn");
     } // UseItem
 
     // determines how close the bubble is to popping
     public string GenerateForesightText()
     {
-        // intialization
+        // initialization
         string textToPrint;
 
         // if bubble is not going to pop on next hit
-        if (GameManager.GetComponent<GameManager>().GetBubbleStatus() > 1)
+        if (_gameManager.GetComponent<GameManager>().GetBubbleStatus() > 1)
         {
             textToPrint = "Bubble will not pop on next throw.";
         }
